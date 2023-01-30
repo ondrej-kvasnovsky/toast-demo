9ee6b2e0ad8bad2ad5ff97a00b467519fa10e3aa
Ondrej Kvasnovsky <ondrej.kvasnovsky@gmail.com>
2023-01-30 11:49:32 -0800

    Initial Commit

---
+++ ToastDemo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ToastDemo
+//
+//  Created by Ondrej Kvasnovsky on 1/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
